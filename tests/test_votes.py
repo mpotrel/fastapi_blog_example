2cc3603ae2e9f528722e7d6025444b3dfb480b23
ManuPotrel <71026308+ManuPotrel@users.noreply.github.com>
2023-03-09 22:20:12 +0100

    slight formatting

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -26,7 +26,10 @@ def test_upvote_twice(authorized_client, test_users, test_posts):
     authorized_client.post("/votes", json=data)  # First vote
     response = authorized_client.post("/votes", json=data)  # Second vote
     assert response.status_code == 409
-    assert response.json().get("detail") == f"User {test_users[0]['id']} has already upvoted post {second_user_post_id}"
+    assert (
+        response.json().get("detail")
+        == f"User {test_users[0]['id']} has already upvoted post {second_user_post_id}"
+    )
     assert get_post_votes(authorized_client, second_user_post_id) == 1
 
 
@@ -37,7 +40,7 @@ def test_remove_upvote(authorized_client, test_users, test_posts):
     authorized_client.post("/votes", json=data)  # Upvote
     assert get_post_votes(authorized_client, second_user_post_id) == 1
     data["dir"] = 0
-    response = authorized_client.post("/votes", json=data) # Downvote
+    response = authorized_client.post("/votes", json=data)  # Downvote
     assert get_post_votes(authorized_client, second_user_post_id) == 0
     assert response.status_code == 201
     assert response.json().get("message") == "Successfully removed upvote"
@@ -48,11 +51,14 @@ def test_downvote_twice(authorized_client, test_users, test_posts):
     data = {"post_id": second_user_post_id, "dir": 1}
     authorized_client.post("/votes", json=data)  # Upvote
     data["dir"] = 0
-    authorized_client.post("/votes", json=data) # Downvote
-    response = authorized_client.post("/votes", json=data) # Downvote again
+    authorized_client.post("/votes", json=data)  # Downvote
+    response = authorized_client.post("/votes", json=data)  # Downvote again
     assert get_post_votes(authorized_client, second_user_post_id) == 0
     assert response.status_code == 409
-    assert response.json().get("detail") == f"User {test_users[0]['id']} has not upvoted post {second_user_post_id}"
+    assert (
+        response.json().get("detail")
+        == f"User {test_users[0]['id']} has not upvoted post {second_user_post_id}"
+    )
 
 
 def test_remove_upvote_without_vote(authorized_client, test_users, test_posts):
@@ -61,7 +67,10 @@ def test_remove_upvote_without_vote(authorized_client, test_users, test_posts):
     response = authorized_client.post("/votes", json=data)
     assert get_post_votes(authorized_client, second_user_post_id) == 0
     assert response.status_code == 409
-    assert response.json().get("detail") == f"User {test_users[0]['id']} has not upvoted post {second_user_post_id}"
+    assert (
+        response.json().get("detail")
+        == f"User {test_users[0]['id']} has not upvoted post {second_user_post_id}"
+    )
 
 
 @pytest.mark.parametrize("dir", [0, 1])
@@ -75,7 +84,9 @@ def test_unauthorized_vote(client, test_posts, dir):
 @pytest.mark.parametrize("dir", [0, 1])
 def test_vote_non_existing_post(authorized_client, dir):
     fake_post_id = 666
-    response = authorized_client.post("/votes", json={"post_id": fake_post_id, "dir": dir})
+    response = authorized_client.post(
+        "/votes", json={"post_id": fake_post_id, "dir": dir}
+    )
     assert response.status_code == 404
     assert response.json().get("detail") == f"Post with id {fake_post_id} was not found"
 
